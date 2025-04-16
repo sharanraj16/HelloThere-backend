8b15a12ce66c4546a639f366093b29eeea3880fa
sharanraj16 <sharanraj.sivakumar@students.codeinstitute.net.test-google-a.com>
2025-04-16 10:56:17 +0200

    comments app is added

--- comments/views.py
+++ comments/views.py
@@ -23,5 +23,3 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
-
-# Create your views here.
